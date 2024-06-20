66e1fd1ac2b6571f4224b0900157ed472bb59d9a
Murat Kertiev <kertiev1107@gmail.com>
2024-06-20 15:49:27 +0300

    Созданы модели, миграции не выполнены

---
+++ product_shop/shop/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    class Meta:
+        ordering = ['name',]
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    image = models.ImageField()
+    parent_category = models.ForeignKey(Category,
+                                        related_name='subcategories',
+                                        on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'подкатегория'
+        verbose_name_plural = 'подкатегории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    subcategory = models.ForeignKey(Subcategory,
+                                    on_delete=models.CASCADE,
+                                    related_name='products')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name',]
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
